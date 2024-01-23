1a27c86714ac54d4a0797aad5a5cdcb2e6fec554
eladforg <elad.forgash@gmail.com>
2024-01-23 07:05:06 +0200

    added test_add_and_remove__item_to_cart

--- pages/products_page.py
+++ pages/products_page.py
@@ -5,21 +5,47 @@ from pages.basePage import BasePage
 
 class ProductsPage(BasePage):
 
-    PRODUCT_ITEMS = (By.CSS_SELECTOR, ".inventory_item")
+    LIST_PRODUCT_ITEMS = (By.CSS_SELECTOR, ".inventory_item")
     FIRST_ITEM = (By.ID, "item_4_title_link")
+    LIST_ADD_TO_CART_BTNS = (By.CSS_SELECTOR, "button.btn")
+    FILTER_BTN = (By.TAG_NAME, "select")
+    CART_BTN = (By.CSS_SELECTOR, "a.shopping_cart_link")
+    CART_BADGE = (By.CSS_SELECTOR, ".shopping_cart_badge")
+    BURGER_BTN = (By.ID, "react-burger-menu-btn")
+    MENU_ALL_ITEMS_BTN = (By.ID, "inventory_sidebar_link")
+    MENU_ABOUT_BTN = (By.ID, "about_sidebar_link")
+    MENU_LOGOUT_BTN = (By.ID, "logout_sidebar_link")
+    MENU_RESET_BTN = (By.ID, "reset_sidebar_link")
+
 
     def __init__(self, driver):
         super().__init__(driver)
 
 
     def check_products_displayed(self):
-        return self.return_elements_amount(self.PRODUCT_ITEMS)
+        return self.return_elements_amount(self.LIST_PRODUCT_ITEMS)
 
 
-    def check_products_data(self):
-        products = self.return_elements(self.PRODUCT_ITEMS)
-        for item in products:
-            print(item)
+    def products_list(self):
+        products = self.return_elements(self.LIST_PRODUCT_ITEMS)
+        return products
+
+
+    def add_to_cart_btns_list(self):
+        return self.return_elements(self.LIST_ADD_TO_CART_BTNS)
+
+
+    def add_remove_item_to_cart(self, btn_number):
+        buttons = self.add_to_cart_btns_list()
+        print(f"button number {buttons[btn_number].text}")
+        if 0 <= btn_number < len(buttons):
+            buttons[btn_number].click()
+        else:
+            print(f"button number {btn_number} is out of bounds")
+
+
+    def check_cart_badge(self):
+        return self.get_text(self.CART_BADGE)
 
 
     def enter_product_page(self):
--- tests/test_products_page.py
+++ tests/test_products_page.py
@@ -1,5 +1,7 @@
 import time
 
+import allure
+
 from pages.loginPage import LoginPage
 from pages.products_page import ProductsPage
 from pages.specific_product_page import SpecificProductPage
@@ -8,13 +10,29 @@ from tests.test_base import TestBase
 
 class TestsOfProductsPage(TestBase):
 
+    @allure.description("checking price is displayed for every item")
     def test_products_data(self, login):
         # p_login = LoginPage(self.driver)
         # p_login.login("standard_user", "secret_sauce")
         p_products = ProductsPage(self.driver)
-        p_products.check_products_data()
-        p_products.enter_product_page()
-        time.sleep(5)
-        p_item = SpecificProductPage(self.driver)
-        p_item.click_back()
-        p_products.check_products_data()
+        products = p_products.products_list()
+        for item in products:
+            assert "$" in item.text, "it seems price is not displayed"
+
+
+
+    @allure.description("add and remove items to cart and check cart badge number")
+    def test_add_and_remove__item_to_cart(self, login):
+        p_products = ProductsPage(self.driver)
+        p_products.add_remove_item_to_cart(2)
+        time.sleep(1)
+        badge = p_products.check_cart_badge()
+        assert badge=="1"
+        p_products.add_remove_item_to_cart(3)
+        time.sleep(1)
+        badge = p_products.check_cart_badge()
+        assert badge == "2"
+        p_products.add_remove_item_to_cart(3)
+        time.sleep(1)
+        badge = p_products.check_cart_badge()
+        assert badge == "1"
